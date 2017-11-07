ddcc2bba6c6d506cf82be31cc63684deeb4b45af
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2017-11-07 10:44:40 -0700

    touch up test script and dotenv with yuri

--- script/run_test_suite.sh
+++ script/run_test_suite.sh
@@ -5,11 +5,23 @@ git clone https://github.com/strongmind/canvas-lms.git
 cd canvas-lms
 git checkout test-master
 cp docker-compose/config/* ./config/
-echo "COMPOSE_FILE=docker-compose.yml:docker-compose.override.yml:docker-compose/selenium.override.yml:docker-compose.local.yml" > .env
+echo "COMPOSE_FILE=docker-compose.yml:docker-compose.override.yml:docker-compose.local.yml:docker-compose/selenium.override.yml:docker-compose/js-tests.override.yml" > .env
 chown -R 9999:9999 ../canvas-lms
 docker-compose down
 docker rm -f $(docker ps -a -q)
 docker rmi -f $(docker images -q)
+
+cat  > docker-compose.local.yml << EOF
+version: '2'
+services:
+  web:
+    environment:
+      CANVAS_LMS_ADMIN_EMAIL: canvas@example.edu
+      CANVAS_LMS_ADMIN_PASSWORD: canvas-docker
+      CANVAS_LMS_ACCOUNT_NAME: SMDockerized
+      CANVAS_LMS_STATS_COLLECTION: 3
+      COVERAGE: 1
+EOF
 
 cat  > config/selenium.yml << EOF
 test:
@@ -18,12 +30,13 @@ test:
   browser: chrome
 EOF
 
-TS=$(date +%FT%T)
+ZONE="America/Phoenix"
+TS=$(date +%Hh-%Mm-%F)
 
 docker-compose build
 docker-compose run --rm web bundle install
 docker-compose run --rm web bundle exec rake db:create db:initial_setup canvas:compile_assets_dev
-docker-compose run --rm web bundle exec rake db:create
-docker-compose run --rm web bundle exec rake db:migrate
+docker-compose run -e "RAILS_ENV=test" --rm web bundle exec rake db:create
+docker-compose run -e "RAILS_ENV=test" --rm web bundle exec rake db:migrate
 docker-compose up -d
 docker-compose run -d web bundle exec rspec spec --format documentation  --out test_results/rspec-$TS.md
