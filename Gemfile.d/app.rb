a67447e34447c974230d4c5f66a6217961bd06b4
John Vanbuskirk <john.vanbuskirk@strongmind.com>
2018-01-04 14:42:59 -0700

    new hash for switchman gem

--- Gemfile.d/app.rb
+++ Gemfile.d/app.rb
@@ -37,7 +37,7 @@ gem 'authlogic', '3.6.0'
 gem 'active_model-better_errors', '1.6.7', require: 'active_model/better_errors'
 gem 'dynamic_form', '1.1.4', require: false
 gem 'rails-patch-json-encode', '0.0.1'
-gem 'switchman', '1.11.4',  github: 'StrongMind/switchman', ref: '6328fc680c9daad197b4bdebec476e9f0e476208'
+gem 'switchman', '1.11.4',  github: 'StrongMind/switchman', ref: 'f0587d8fd990e1479e6e6155bb1901f1bb25dd69'
 
   gem 'open4', '1.3.4', require: false
 gem 'switchman-inst-jobs'
